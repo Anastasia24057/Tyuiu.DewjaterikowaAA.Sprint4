a249bcaa59c378546353cf1331c85cc2411f0dd6
Anastasia24057 <nastya.dewa@gmail.com>
2025-10-27 23:29:22 +0500

    Task1

---
+++ Tyuiu.DewjaterikowaAA.Sprint4.Task1.V12.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.DewjaterikowaAA.Sprint4.Task1.V12.Lib;
+
+namespace Tyuiu.DewjaterikowaAA.Sprint4.Task1.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Calculate()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 4, 8, 6, 4, 9, 5, 8, 7, 8, 4, 7, 6, 8 };
+            int wait = 2205;
+            int res = ds.Calculate(numsArray);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DewjaterikowaAA.Sprint4.Task1.V12/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.DewjaterikowaAA.Sprint4.Task1.V12.Lib;
+DataService ds = new DataService();
+
+Console.Title = "Спринт #4 | Выполнила: Девятерикова А.А. | ИИПБ - 25 - 1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                            *");
+Console.WriteLine("* Задание #1                                                              *");
+Console.WriteLine("* Вариант #12                                                             *");
+Console.WriteLine("* Выполнила: Девятерикова Анастасия Андреевна | ИИПБ - 25 - 1             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Подсчитать произведение нечётных элементов массива, запрашивая с        *");
+Console.WriteLine("* клавиатуры данные.                                                      *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int[] numsArray = new int[13];
+
+Console.WriteLine("Введите 13 элементов массива (числа в диапазоне от 4 до 9)");
+for (int i = 0; i < 13;)
+{
+    numsArray[i] = Convert.ToInt32(Console.ReadLine());
+    if (numsArray[i] >= 4 && numsArray[i] <= 9)
+    { i++; }
+    else
+    { Console.WriteLine("Значение не входит в промежуток от 4 до 9, введите еще раз"); }
+}
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Произведение нечётных элементов массива = " + ds.Calculate(numsArray));
+Console.ReadKey();
+
+
+
+
+
+
+
+
