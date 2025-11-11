bd0755b435acbb8ed09571aeada4f2055e914ea5
Anastasia24057 <nastya.dewa@gmail.com>
2025-11-11 22:29:17 +0500

    Task4

---
+++ Tyuiu.DewjaterikowaAA.Sprint4.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.DewjaterikowaAA.Sprint4.Task4.V30.Lib;
+
+namespace Tyuiu.DewjaterikowaAA.Sprint4.Task4.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Calculate()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = new int[,]
+            {
+                {3, 4, 1},
+                {4, 0, 6},
+                {7, 6, 5}
+            };
+            int[,] wait = new int[,]
+            {
+                {0, 4, 0},
+                {4, 0, 6},
+                {0, 6, 0}
+            };
+            int[,] result = ds.Calculate(matrix);
+            CollectionAssert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.DewjaterikowaAA.Sprint4.Task4.V30/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.DewjaterikowaAA.Sprint4.Task4.V30.Lib;
+DataService ds = new DataService();
+Console.Title = "Спринт #4 | Выполнила: Девятерикова А.А. | ИИПБ - 25 - 1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Двумерные массивы (статистический ввод)                           *");
+Console.WriteLine("* Задание #4                                                              *");
+Console.WriteLine("* Вариант #30                                                             *");
+Console.WriteLine("* Выполнила: Девятерикова Анастасия Андреевна | ИИПБ - 25 - 1             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+Console.WriteLine("* значениями c клавиатуры в диапазоне от 3 до 7. Замените нечётные        *");
+Console.WriteLine("* элементы массива на 0.                                                  *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество строк массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[rows, columns];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.WriteLine($"Введите {i},{j} элемент массива");
+        matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("\nМассив:");
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+Console.WriteLine("****************************************************************************");
+
+int[,] res = ds.Calculate(matrix);
+
+Console.WriteLine("\nИзмененный массив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{res[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+Console.ReadKey();
