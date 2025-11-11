095e7eea4690dcbf4e32814b373bf5235460cf67
Anastasia24057 <nastya.dewa@gmail.com>
2025-11-11 21:45:22 +0500

    Task3

---
+++ Tyuiu.DewjaterikowaAA.Sprint4.Task3.V2/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.DewjaterikowaAA.Sprint4.Task3.V2.Lib;
+int[,] mtrx = new int[5, 5] { { 4, 3, 2, 8, 7 }, { 4, 6, 5, 5, 7 }, { 3, 8, 8, 3, 5 }, { 5, 7, 5, 4, 4 }, { 8, 8, 3, 7, 6 } };
+int rows = mtrx.GetUpperBound(0) + 1;
+int colums = mtrx.Length / rows;
+DataService ds = new DataService();
+Console.Title = "Спринт #4 | Выполнила: Девятерикова А.А. | ИИПБ - 25 - 1";
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Двумерные массивы (статистический ввод)                           *");
+Console.WriteLine("* Задание #3                                                              *");
+Console.WriteLine("* Вариант #2                                                              *");
+Console.WriteLine("* Выполнила: Девятерикова Анастасия Андреевна | ИИПБ - 25 - 1             *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+Console.WriteLine("* статическими значениями в диапазоне от 2 до 8. Найдите максимальный     *");
+Console.WriteLine("* элемент в первой строке массива.                                        *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("Массив:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < colums; j++)
+    {
+        Console.Write($"{mtrx[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+Console.WriteLine("****************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+Console.WriteLine("****************************************************************************");
+int res = ds.Calculate(mtrx);
+Console.WriteLine("Максимальный элемент первой строки = " + res);
+Console.ReadLine();
