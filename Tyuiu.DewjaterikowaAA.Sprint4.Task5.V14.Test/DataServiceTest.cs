02a6e87b901369b45502c09c362f76687f549ddf
Anastasia24057 <nastya.dewa@gmail.com>
2025-11-12 19:50:46 +0500

    Task5

--- Tyuiu.DewjaterikowaAA.Sprint4.Task5.V14.Test/DataServiceTest.cs
+++ Tyuiu.DewjaterikowaAA.Sprint4.Task5.V14.Test/DataServiceTest.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.DewjaterikowaAA.Sprint4.Task5.V14.Test
         public void Calculate()
         {
             DataService ds = new DataService();
-            int[,] mas2 = new int[3, 3] { { 1, 2, -5 }, { 3, 4, -3 }, { 7, 2, 3 } };
+            int[,] mas2 = new int[3, 3] { { 1, 3, -5 }, { 3, 4, -3 }, { 7, 2, 3 } };
             int res = ds.Calculate(mas2);
             int wait = 7;
             Assert.AreEqual(wait, res);
